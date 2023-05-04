e12312280fe9da3713951b97d7a1c87bedf54c4f
Pearson <spearson@lwsd.org>
2023-05-03 19:47:15 -0700

    fixed password

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://spearson1986:M@rktrain1286@cluster0.ezrovhf.mongodb.net/freelancerDB?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb+srv://spearson1986:M%rktrain1286@cluster0.ezrovhf.mongodb.net/freelancerDB?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,

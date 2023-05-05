a612fea2d506b5add329f748c417d2172c5069fc
Pearson <spearson@lwsd.org>
2023-05-04 18:59:19 -0700

    changes

--- server/server.js
+++ server/server.js
@@ -19,9 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build/')));
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/'));
+    res.sendFile(path.join(__dirname, '../client/build/'));
   });
 }
 
